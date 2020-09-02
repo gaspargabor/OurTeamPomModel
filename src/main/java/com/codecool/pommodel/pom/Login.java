1cbb5c08b2404310ff1fd9d9db8e8164f99e481b
zsoltion-cc <55063936+zsoltion-cc@users.noreply.github.com>
2020-09-02 09:45:54 +0200

    Code Cleanup

--- src/main/java/com/codecool/pommodel/pom/Login.java
+++ src/main/java/com/codecool/pommodel/pom/Login.java
@@ -1,6 +1,5 @@
 package com.codecool.pommodel.pom;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
--- src/test/java/EditIssueCases.java
+++ src/test/java/EditIssueCases.java
@@ -3,7 +3,6 @@ import com.codecool.pommodel.pom.Login;
 import com.codecool.pommodel.pom.TestIssueEdit;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
